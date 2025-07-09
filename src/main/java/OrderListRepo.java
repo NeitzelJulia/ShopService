544d9138578ecfcaeb982565f5720ff0f0222e62
NeitzelJulia <julia.neitzel86@gmail.com>
2025-07-09 13:23:07 +0200

    OrderRepoInterface

--- src/main/java/OrderListRepo.java
+++ src/main/java/OrderListRepo.java
@@ -7,18 +7,22 @@ public class OrderListRepo implements OrderRepoInterface{
 
     Map<String, Order> orders = new HashMap<>();
 
+    @Override
     public void addOrder(Order order) {
         orders.put(order.id(), order);
     }
 
+    @Override
     public Order getOrderById(String id) {
         return orders.get(id);
     }
 
+    @Override
     public boolean deleteOrderById(String id) {
         return orders.remove(id) != null;
     }
 
+    @Override
     public List<Order> getAllOrders() {
         return new ArrayList<>(orders.values());
     }
