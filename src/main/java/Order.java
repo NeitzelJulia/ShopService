9da592a7092d29b1339a13642e5766e284d77017
NeitzelJulia <julia.neitzel86@gmail.com>
2025-07-16 10:24:49 +0200

    add OrderStatus enum to Order

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -2,22 +2,29 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public record Order(String id, Map<Product, Integer> products) {
+public record Order(
+        String id,
+        Map<Product, Integer> products,
+        OrderStatus status) {
 
     public static Order empty(String id) {
-        return new Order(id, Collections.emptyMap());
+        return new Order(id, Collections.emptyMap(), OrderStatus.PROCESSING);
     }
 
     public Order addProduct(Product product, int quantity) {
         Map <Product, Integer> copy =  new HashMap<>(products);
         copy.put(product, quantity);
-        return new Order(id, Collections.unmodifiableMap(copy));
+        return new Order(id, Collections.unmodifiableMap(copy), status);
     }
 
     public Order removeProduct(Product product) {
         Map <Product, Integer> copy =  new HashMap<>(products);
         copy.remove(product);
-        return new Order(id, Collections.unmodifiableMap(copy));
+        return new Order(id, Collections.unmodifiableMap(copy), status);
+    }
+
+    public Order withStatus(OrderStatus newStatus) {
+        return new Order(id, products, newStatus);
     }
 
     public long totalInCents() {
